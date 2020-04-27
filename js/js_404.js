66dc8ba3e10913018039699f288674c32d5f6c7e
ShellyBoden <s110619046@stu.ntue.edu.tw>
2020-04-27 13:32:16 +0800

    新增404頁面

---
+++ js/js_404.js
@@ -0,0 +1,19 @@
+//設定倒數秒數
+var t = 6;
+
+//顯示倒數秒收
+function showTime()
+{
+    t -= 1;
+    document.getElementById('timer').innerHTML= t;
+    
+    if(t==1){
+        location.href='/index.html';
+    }
+    
+    //每秒執行一次,showTime()
+    setTimeout("showTime()",1000);
+}
+
+//執行showTime()
+showTime();
